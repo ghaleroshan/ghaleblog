ed5d0c4c453d5a3ac858a5430526d0d738cb48ad
ghaleroshan <roshan.ghale@practifi.com>
2022-10-11 22:47:17 +1100

    fix build issues

--- _data/site.js
+++ _data/site.js
@@ -1,12 +1,20 @@
 module.exports = {
   meta: {
     title: "Roshan Ghale",
-    description: "I'm Roshan, Software Developer",
+    description: "Software Developer",
     lang: "en",
     siteUrl: "https://ghaleroshan.com/",
   },
+  feed: { // used in feed.xml.njk
+    subtitle: "Roshan Ghale, Software Developer",
+    filename: "atom.xml",
+    path: "/atom.xml",
+    id: "https://ghaleroshan.com/",
+    authorName: "Roshan Ghale",
+    authorEmail: "ghaleroshan@gmail.com"
+  },
   hero: { // used in hero section of main page ie. index.html.njk
-    title: "I'm Roshan, Software Developer at Practifi",
-    description: "I am Sydney based software developer with almost 2 years of experience."
+    title: "Hi, I am Roshan Ghale, a Software Engineer",
+    description: "Passionate Software Engineer skilled with Javscript, React, Node, as well as Salesforce Development. Currently working as a Software Developer at Practifi."
   }
 }
